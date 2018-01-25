6396725270e7216f4d4bed7fff1015b6f50b955b
Zoltan Varga <vargaz@gmail.com>
2018-01-25 17:24:29 -0500

    [ci] Delete the install dir under /tmp on exit.

--- scripts/ci/run-jenkins.sh
+++ scripts/ci/run-jenkins.sh
@@ -6,6 +6,8 @@ env
 # The generated binaries are not relocateable so we need to install them in a global prefix
 MXE_PREFIX=/tmp/mxe-$GIT_COMMIT
 
+trap "rm -rf $MXE_PREFIX" EXIT
+
 make MXE_TARGETS='x86_64-w64-mingw32.static' gcc cmake zlib pthreads dlfcn-win32 mman-win32 PREFIX=$MXE_PREFIX OS_SHORT_NAME='disable-native-plugins'
 make MXE_TARGETS='i686-w64-mingw32.static' gcc cmake zlib pthreads dlfcn-win32 mman-win32 PREFIX=$MXE_PREFIX OS_SHORT_NAME='disable-native-plugins'
 
