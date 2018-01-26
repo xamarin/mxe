b9cbb53541a0e10fe4fe81f22bd586cb9cdc922a
Zoltan Varga <vargaz@gmail.com>
2018-01-25 20:04:33 -0500

    [ci] Add a jenkins build script.

---
+++ scripts/ci/run-jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+echo "ENVIRONMENT:"
+env
+
+# The generated binaries are not relocateable so we need to install them in a global prefix
+MXE_PREFIX=/tmp/mxe-$GIT_COMMIT
+
+trap "rm -rf $MXE_PREFIX" EXIT
+
+make MXE_TARGETS='x86_64-w64-mingw32.static' gcc cmake zlib pthreads dlfcn-win32 mman-win32 PREFIX=$MXE_PREFIX OS_SHORT_NAME='disable-native-plugins'
+make MXE_TARGETS='i686-w64-mingw32.static' gcc cmake zlib pthreads dlfcn-win32 mman-win32 PREFIX=$MXE_PREFIX OS_SHORT_NAME='disable-native-plugins'
+
+tar cvzf mxe-osx-$GIT_COMMIT.tar.gz $MXE_PREFIX
